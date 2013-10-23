ca56f95a4e8bc7191198f460beea3f11c8e2f5de
Lecho <leszekwach@gmail.com>
2013-10-23 20:24:24 +0200

    Library project.

---
+++ hellocharts-library/src/lecho/lib/hellocharts/MainActivity.java
@@ -0,0 +1,22 @@
+package lecho.lib.hellocharts;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
