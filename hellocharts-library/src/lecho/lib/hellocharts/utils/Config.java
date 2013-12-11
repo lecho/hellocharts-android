9655101795fad0b7ddb8d37f1d3b1852244f9d74
lecho <leszekwach@gmail.com>
2013-12-11 22:11:10 +0100

    Change defoult animation duration to 1500ms.

--- hellocharts-library/src/lecho/lib/hellocharts/utils/Config.java
+++ hellocharts-library/src/lecho/lib/hellocharts/utils/Config.java
@@ -2,7 +2,7 @@ package lecho.lib.hellocharts.utils;
 
 public abstract class Config {
 
-	public static final long DEFAULT_ANIMATION_DURATION = 1000;
+	public static final long DEFAULT_ANIMATION_DURATION = 1500;
 	public static final float DEFAULT_TOUCH_SCALE = 1.75f;
 
 }
