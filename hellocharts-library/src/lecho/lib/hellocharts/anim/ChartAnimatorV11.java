084acbb3cd9aecabc14866360430bc7ab2185e45
lecho <leszekwach@gmail.com>
2014-02-27 22:46:39 +0100

    Refactored ChartAnimatorV11 to use ValueAnimator.

--- hellocharts-library/src/lecho/lib/hellocharts/anim/ChartAnimatorV11.java
+++ hellocharts-library/src/lecho/lib/hellocharts/anim/ChartAnimatorV11.java
@@ -3,7 +3,6 @@ package lecho.lib.hellocharts.anim;
 import lecho.lib.hellocharts.LineChart;
 import android.animation.Animator;
 import android.animation.Animator.AnimatorListener;
-import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.annotation.SuppressLint;
@@ -11,31 +10,28 @@ import android.view.animation.LinearInterpolator;
 
 @SuppressLint("NewApi")
 public class ChartAnimatorV11 implements ChartAnimator, AnimatorListener, AnimatorUpdateListener {
-	@SuppressWarnings("unused")
-	private float mScale;
-	private ObjectAnimator mObjectAnimator;
+	private ValueAnimator mAnimator;
 	private final LineChart mChart;
 	private final long mDuration;
 
 	public ChartAnimatorV11(final LineChart chart, final long duration) {
 		mChart = chart;
 		mDuration = duration;
-		mObjectAnimator = ObjectAnimator.ofFloat(this, "mScale", 0.0f, 1.0f);
-		mObjectAnimator.setDuration(mDuration);
-		mObjectAnimator.setInterpolator(new LinearInterpolator());
-		mObjectAnimator.addListener(this);
-		mObjectAnimator.addUpdateListener(this);
+		mAnimator = ValueAnimator.ofFloat(0.0f, 1.0f);
+		mAnimator.setDuration(mDuration);
+		mAnimator.setInterpolator(new LinearInterpolator());
+		mAnimator.addListener(this);
+		mAnimator.addUpdateListener(this);
 	}
 
 	@Override
 	public void startAnimation() {
-		mScale = 0.0f;
-		mObjectAnimator.start();
+		mAnimator.start();
 	}
 
 	@Override
 	public void cancelAnimation() {
-		mObjectAnimator.cancel();
+		mAnimator.cancel();
 	}
 
 	@Override
