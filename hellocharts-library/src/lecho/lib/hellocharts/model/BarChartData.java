f042afd4621dc00b8470502c8532111c2e7f57ab
Lecho <leszekwach@gmail.com>
2014-05-31 22:15:21 +0200

    Calculate boundaries for stacked bar chart

--- hellocharts-library/src/lecho/lib/hellocharts/model/BarChartData.java
+++ hellocharts-library/src/lecho/lib/hellocharts/model/BarChartData.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class BarChartData extends AbstractChartData {
 
 	public List<Bar> bars = Collections.emptyList();
+	public boolean isStacked = false;
 
 	@Override
 	public void calculateBoundaries() {
@@ -13,12 +14,32 @@ public class BarChartData extends AbstractChartData {
 			return;
 		}
 		mBoundaries.set(-0.5f, Float.MIN_VALUE, bars.size() - 0.5f, 0);
+		if (isStacked) {
+			calculateBoundariesStacked();
+		} else {
+			calculateBoundariesDefault();
+		}
+	}
+
+	public void calculateBoundariesDefault() {
 		for (Bar bar : bars) {
 			for (AnimatedValueWithColor animatedValue : bar.animatedValues) {
 				if (animatedValue.value > mBoundaries.top) {
 					mBoundaries.top = animatedValue.value;
 				}
 
+			}
+		}
+	}
+
+	public void calculateBoundariesStacked() {
+		for (Bar bar : bars) {
+			float sum = 0;
+			for (AnimatedValueWithColor animatedValue : bar.animatedValues) {
+				sum += animatedValue.value;
+			}
+			if (sum > mBoundaries.top) {
+				mBoundaries.top = sum;
 			}
 		}
 	}
