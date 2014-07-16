88ea428ac277215943dfadcca029a1188967e920
Lecho <leszekwach@gmail.com>
2014-07-16 22:15:18 +0200

    Renamed ValueFormatter

--- hellocharts-library/src/lecho/lib/hellocharts/model/ValueFormater.java
+++
@@ -1,7 +0,0 @@
-package lecho.lib.hellocharts.model;
-
-public interface ValueFormater {
-
-	public int formatValue(final char[] out, float val, int digits);
-
-}
---
+++ hellocharts-library/src/lecho/lib/hellocharts/model/ValueFormatter.java
@@ -0,0 +1,7 @@
+package lecho.lib.hellocharts.model;
+
+public interface ValueFormatter {
+
+	public int formatValue(final char[] formattedValue, float value, int digits);
+
+}
