1341081c0e631129343a322bf578f9287cbdbd94
Lecho <leszekwach@gmail.com>
2014-07-26 16:38:18 +0200

    Renamed ColumnChart into ColumnChartDataProvider and LineChart into LineChartDataProvider

---
+++ hellocharts-library/src/lecho/lib/hellocharts/ColumnChartDataProvider.java
@@ -0,0 +1,9 @@
+package lecho.lib.hellocharts;
+
+import lecho.lib.hellocharts.model.ColumnChartData;
+
+public interface ColumnChartDataProvider {
+
+	public ColumnChartData getColumnChartData();
+
+}
---
+++ hellocharts-library/src/lecho/lib/hellocharts/LineChartDataProvider.java
@@ -0,0 +1,9 @@
+package lecho.lib.hellocharts;
+
+import lecho.lib.hellocharts.model.LineChartData;
+
+public interface LineChartDataProvider {
+
+	public LineChartData getLineChartData();
+
+}
