c61bcba8ba30b2f76112e83eeb241802857c6ada
Lecho <leszekwach@gmail.com>
2014-08-14 19:11:44 +0200

    Fixed out of bounds error in CastaljauComputator

--- hellocharts-library/src/lecho/lib/hellocharts/util/CasteljauComputator.java
+++ hellocharts-library/src/lecho/lib/hellocharts/util/CasteljauComputator.java
@@ -36,11 +36,11 @@ public class CasteljauComputator {
 		}
 
 		// Copy first raw of points into this.points[0] array.
-		System.arraycopy(points[0], 0, startPoints, 0, pointsNumber);
+		System.arraycopy(startPoints, 0, points[0], 0, pointsNumber);
 
-		for (int i = 1, pointsIndex = pointsNumber; i < curveDegree; ++i, pointsIndex -= 2) {
+		for (int i = 1, pointsIndex = pointsNumber - 2; i < curveDegree; ++i, pointsIndex -= 2) {
 
-			for (int indexX = 0, indexY = 1; indexY < pointsIndex; indexX += 2, indexY += 2) {
+			for (int indexX = 0, indexY = 1; indexY <= pointsIndex; indexX += 2, indexY += 2) {
 
 				// X value.
 				points[i][indexX] = (1 - t) * points[i - 1][indexX] + t * points[i - 1][indexX + 2];
