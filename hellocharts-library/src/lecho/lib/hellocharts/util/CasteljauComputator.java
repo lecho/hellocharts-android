74e9d73f03e61051b7f179687d1ad31ae5145fc1
Lecho <leszekwach@gmail.com>
2014-08-14 21:59:38 +0200

    Fixed CasteljauComputator to use all control points for computations

--- hellocharts-library/src/lecho/lib/hellocharts/util/CasteljauComputator.java
+++ hellocharts-library/src/lecho/lib/hellocharts/util/CasteljauComputator.java
@@ -5,51 +5,54 @@ import android.graphics.PointF;
 /**
  * Iterative implementation of de Casteljau's algorithm for cubic Bezier's curves.
  * 
- * @author Leszek Wach
- * 
  */
 public class CasteljauComputator {
 	private static final int DEFAULT_CURVE_DEGREE = 3;// By default cubic.
 
-	private int curveDegree = DEFAULT_CURVE_DEGREE;
-	private int pointsNumber = (curveDegree + 1) * 2;
-	// TODO: Use one-dimensional array.
-	private float[][] points;
+	private int pointsNum = DEFAULT_CURVE_DEGREE + 1;
+	private int coordsNum = pointsNum * 2;
+	/**
+	 * This implementation uses single dimension array for storing partial results. If you need to see whole triangular
+	 * computation scheme use two-dimensional array when first dimension is indexed by pointsNum(variable i in first FOR
+	 * loop) and second dimension is indexed by coordsNum(variables ix and iy in second FOR loop).
+	 */
+	private float[] points;
 
 	public CasteljauComputator() {
-		points = new float[curveDegree + 1][pointsNumber];
+		points = new float[coordsNum];
 	}
 
 	public CasteljauComputator(int curveDegree) {
 		if (curveDegree < 1) {
 			curveDegree = 1;
 		}
-		this.curveDegree = curveDegree;
-		pointsNumber = (curveDegree + 1) * 2;
-		points = new float[curveDegree + 1][pointsNumber];
+		pointsNum = curveDegree + 1;
+		coordsNum = pointsNum * 2;
+		points = new float[coordsNum];
 	}
 
 	public void computePoint(float t, float[] startPoints, PointF outPoint) {
-		if (startPoints.length != pointsNumber) {
+		if (startPoints.length != coordsNum) {
 			throw new IllegalArgumentException(
 					"Invalid points number, points array should have (curveDegree+1) * 2 values");
 		}
 
 		// Copy first raw of points into this.points[0] array.
-		System.arraycopy(startPoints, 0, points[0], 0, pointsNumber);
+		System.arraycopy(startPoints, 0, points, 0, coordsNum);
 
-		for (int i = 1, pointsIndex = pointsNumber - 2; i < curveDegree; ++i, pointsIndex -= 2) {
+		for (int i = 1, length = coordsNum - 2; i < pointsNum; ++i, length -= 2) {
 
-			for (int indexX = 0, indexY = 1; indexY <= pointsIndex; indexX += 2, indexY += 2) {
+			for (int ix = 0, iy = 1; iy <= length; ix += 2, iy += 2) {
 
 				// X value.
-				points[i][indexX] = (1 - t) * points[i - 1][indexX] + t * points[i - 1][indexX + 2];
+				points[ix] = (1 - t) * points[ix] + t * points[ix + 2];
 				// Y value.
-				points[i][indexY] = (1 - t) * points[i - 1][indexY] + t * points[i - 1][indexY + 2];
+				points[iy] = (1 - t) * points[iy] + t * points[iy + 2];
 
 			}
+
 		}
 
-		outPoint.set(points[curveDegree - 1][0], points[curveDegree - 1][1]);
+		outPoint.set(points[0], points[1]);
 	}
 }
