86fed192d699a114fac2f5d7bd333ab8ee98934c
Lecho <leszekwach@gmail.com>
2014-08-23 13:39:52 +0200

    Chart interface java docs

--- hellocharts-library/src/lecho/lib/hellocharts/Chart.java
+++ hellocharts-library/src/lecho/lib/hellocharts/Chart.java
@@ -8,8 +8,16 @@ import lecho.lib.hellocharts.model.Viewport;
 import lecho.lib.hellocharts.renderer.AxesRenderer;
 import lecho.lib.hellocharts.renderer.ChartRenderer;
 
+/**
+ * Interface for all charts. Every chart must implements this interface but chart doesn't really have to extends View or
+ * ViewGroup class. It can be any java class for example chart that only draw on in-memory bitmap and saves it on sd
+ * card.
+ */
 public interface Chart {
 
+	/**
+	 * Returns generic chart data. For specific class call get*ChartData method from data provider implementation.
+	 */
 	public ChartData getChartData();
 
 	public ChartRenderer getChartRenderer();
@@ -20,52 +28,146 @@ public interface Chart {
 
 	public ChartTouchHandler getTouchHandler();
 
+	/**
+	 * Updates chart data with given scale. Called during chart data animation update.
+	 */
 	public void animationDataUpdate(float scale);
 
+	/**
+	 * Called when data animation finished.
+	 */
 	public void animationDataFinished(boolean isFinishedSuccess);
 
+	/**
+	 * Starts chart data animation. Before you call this method you should change target values of chart data.
+	 */
 	public void startDataAnimation();
 
+	/**
+	 * Set listener for data animation to be notified when data animation started and finished.
+	 */
 	public void setDataAnimationListener(ChartAnimationListener animationListener);
 
+	/**
+	 * Set listener for viewport animation to be notified when viewport animation started and finished.
+	 */
 	public void setViewportAnimationListener(ChartAnimationListener animationListener);
 
+	/**
+	 * Set listener for current viewport changes. It will be called when viewport change either by gesture or
+	 * programmatically. Warning! This method works only for preview charts. It is intentionally disabled for other
+	 * types of charts to avoid unnecessary method calls during invalidation.
+	 * 
+	 */
 	public void setViewportChangeListener(ViewportChangeListener viewportChangeListener);
 
+	/**
+	 * Programmatically call touch listener for selected value. Indexes of selected value must be valid or
+	 * IndexOutOfBoundsException will be thrown.
+	 */
 	public void callChartTouchListener(SelectedValue selectedValue);
 
+	/**
+	 * Returns true if chart is interactive.
+	 * 
+	 * @see #setInteractive(boolean)
+	 */
 	public boolean isInteractive();
 
+	/**
+	 * Set true to allow user use touch gestures. If set to false user will not be able zoom, scroll or select/touch
+	 * value. By default true.
+	 */
 	public void setInteractive(boolean isInteractive);
 
+	/**
+	 * Returns true if pitch to zoom and double tap zoom is enabled.
+	 * 
+	 * @see #setZoomEnabled(boolean)
+	 */
 	public boolean isZoomEnabled();
 
+	/**
+	 * Set true to enable zoom, false to disable, by default true;
+	 */
 	public void setZoomEnabled(boolean isZoomEnabled);
 
+	/**
+	 * Returns current zoom type for this chart.
+	 * 
+	 * @see #setZoomType(int)
+	 */
 	public int getZoomType();
 
+	/**
+	 * Set zoom type, available options: ChartZoomer.ZOOM_HORIZONTAL_AND_VERTICAL, ChartZoomer.ZOOM_HORIZONTAL,
+	 * ChartZoomer.ZOOM_VERTICAL. By default ChartZoomer.ZOOM_HORIZONTAL_AND_VERTICAL.
+	 */
 	public void setZoomType(int zoomType);
 
+	/**
+	 * Return true if chart value can be touched.
+	 * 
+	 * @see #setValueTouchEnabled(boolean)
+	 */
 	public boolean isValueTouchEnabled();
 
+	/**
+	 * Set true if you want allow user to click value on chart, set false to disable that option. By default true.
+	 */
 	public void setValueTouchEnabled(boolean isValueTouchEnabled);
 
-	public void setMaxViewport(Viewport maxViewport);
-
+	/**
+	 * Returns maximum viewport for this chart. Don't modify it directly, use {@link #setMaxViewport(Viewport)} instead.
+	 * 
+	 * @see #setMaxViewport(Viewport)
+	 */
 	public Viewport getMaxViewport();
 
-	public void setViewport(Viewport targetViewport, boolean isAnimated);
+	/**
+	 * Set maximum viewport. That lets you change how much of the chart area is really used by data. If you set bigger
+	 * maximum viewport data will be more concentrate and there will be more empty spaces on sides.
+	 */
+	public void setMaxViewport(Viewport maxViewport);
 
+	/**
+	 * Returns current viewport. Don't modify it directly, use {@link #setViewport(Viewport, boolean)} instead.
+	 * 
+	 * @see #setViewport(Viewport, boolean)
+	 */
 	public Viewport getViewport();
 
+	/**
+	 * Sets current viewport. If isAnimated is true chart will be animated during viewport changes.
+	 */
+	public void setViewport(Viewport targetViewport, boolean isAnimated);
+
+	/**
+	 * Programatically zoom chart to given point(its viewport point). Negative zoomAmount parameter means zoom out.
+	 */
 	public void zoom(float x, float y, float zoomAmout);
 
+	/**
+	 * Return true if value selection mode is enabled.
+	 * 
+	 * @see #setValueSelectionEnabled(boolean)
+	 */
 	public boolean isValueSelectionEnabled();
 
+	/**
+	 * Set true if you want value selection with touch - value will stay selected until you touch somewhere else on the
+	 * chart area. By default false and value is automatically unselected when user stop pressing on it.
+	 */
 	public void setValueSelectionEnabled(boolean isValueSelectionEnabled);
 
+	/**
+	 * Select single value on chart. If indexes are not valid IndexOutOfBoundsException will be thrown.
+	 */
 	public void selectValue(SelectedValue selectedValue);
 
+	/**
+	 * Return currently selected value indexes.
+	 */
 	public SelectedValue getSelectedValue();
 
 }
