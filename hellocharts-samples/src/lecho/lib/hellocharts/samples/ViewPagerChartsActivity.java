dacef76231e2ee35ee8b9cdf104139663c63b876
Lecho <leszekwach@gmail.com>
2014-10-01 21:29:40 +0200

    Revert "Renamed modules to use CamelCase"
    This reverts commit 383d02c3c6de81a201f64f3a8aabdb2a80c8dee6.

--- hellocharts-samples/src/lecho/lib/hellocharts/samples/ViewPagerChartsActivity.java
+++ hellocharts-samples/src/lecho/lib/hellocharts/samples/ViewPagerChartsActivity.java
@@ -32,6 +32,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
