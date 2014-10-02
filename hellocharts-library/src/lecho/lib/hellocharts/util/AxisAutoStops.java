c12ade8db75127c8b2f6fc8306888526ef7b8dcd
Lecho <leszekwach@gmail.com>
2014-10-02 22:40:33 +0200

    Rename AxisStops into AxisAutoStops

--- hellocharts-library/src/lecho/lib/hellocharts/util/AxisAutoStops.java
+++ hellocharts-library/src/lecho/lib/hellocharts/util/AxisAutoStops.java
@@ -2,10 +2,9 @@ package lecho.lib.hellocharts.util;
 
 /**
  * A simple class representing axis label values used only for auto generated axes.
- * 
  */
-public class AxisStops {
-	public float[] stops = new float[] {};
-	public int numStops;
-	public int decimals;
+public class AxisAutoStops {
+    public float[] stops = new float[]{};
+    public int numStops;
+    public int decimals;
 }
