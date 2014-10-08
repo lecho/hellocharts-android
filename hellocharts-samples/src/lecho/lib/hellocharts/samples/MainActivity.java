cb2dd2d604561813b1a1ad24ab602ca9df6cea63
Lecho <leszekwach@gmail.com>
2014-10-08 21:09:12 +0200

    Improved samples layout with small charts icons

--- hellocharts-samples/src/lecho/lib/hellocharts/samples/MainActivity.java
+++ hellocharts-samples/src/lecho/lib/hellocharts/samples/MainActivity.java
@@ -3,6 +3,14 @@ package lecho.lib.hellocharts.samples;
 import java.util.ArrayList;
 import java.util.List;
 
+import lecho.lib.hellocharts.view.AbstractChartView;
+import lecho.lib.hellocharts.view.BubbleChartView;
+import lecho.lib.hellocharts.view.ColumnChartView;
+import lecho.lib.hellocharts.view.LineChartView;
+import lecho.lib.hellocharts.view.PieChartView;
+import lecho.lib.hellocharts.view.PreviewColumnChartView;
+import lecho.lib.hellocharts.view.PreviewLineChartView;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +24,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -145,22 +154,30 @@ public class MainActivity extends ActionBarActivity {
 		private List<ChartSampleDescription> generateSamplesDescriptions() {
 			List<ChartSampleDescription> list = new ArrayList<MainActivity.ChartSampleDescription>();
 
-			list.add(new ChartSampleDescription("Line Chart", ""));
-			list.add(new ChartSampleDescription("Column Chart", ""));
-			list.add(new ChartSampleDescription("Pie Chart", ""));
-			list.add(new ChartSampleDescription("Bubble Chart", ""));
+			list.add(new ChartSampleDescription("Line Chart", "", ChartType.LINE_CHART));
+			list.add(new ChartSampleDescription("Column Chart", "", ChartType.COLUMN_CHART));
+			list.add(new ChartSampleDescription("Pie Chart", "", ChartType.PIE_CHART));
+			list.add(new ChartSampleDescription("Bubble Chart", "", ChartType.BUBBLE_CHART));
 			list.add(new ChartSampleDescription("Preview Line Chart",
-					"Control Line Chart viewport with another Line Chart "));
+					"Control line chart viewport with another line chart.", ChartType.PREVIEW_LINE_CHART));
 			list.add(new ChartSampleDescription("Preview Column Chart",
-					"Control Column Chart viewport with another Column Chart"));
-			list.add(new ChartSampleDescription("Combo Line/Column Chart", "Combo chart with lines and columns"));
+					"Control column chart viewport with another column chart.", ChartType.PREVIEW_COLUMN_CHART));
+			list.add(new ChartSampleDescription("Combo Line/Column Chart", "Combo chart with lines and columns.",
+					ChartType.OTHER));
 			list.add(new ChartSampleDescription("Line/Column Chart Dependency",
-					"LineChart responds(with animation) to ColumnChart value selection"));
-			list.add(new ChartSampleDescription("Tempo Chart Sample", "Example of reverted axis with time format mm:ss"));
-			list.add(new ChartSampleDescription("Speed Chart Sample", "Exapmle of chart with axes inside chart area"));
-			list.add(new ChartSampleDescription("Good/Bad Chart Sample", "Example of filled area line chart"));
-			list.add(new ChartSampleDescription("ViewPager with Chart Sample",
-					"Charts within ViewPager can be zoomed and scrolled"));
+					"LineChart responds(with animation) to column chart value selection.", ChartType.OTHER));
+			list.add(new ChartSampleDescription(
+					"Tempo Chart",
+					"Presents tempo and height values on a signle chart. Example of multiple axes and reverted Y axis with time format [mm:ss].",
+					ChartType.OTHER));
+			list.add(new ChartSampleDescription("Speed Chart",
+					"Presents speed and height values on a signle chart. Exapmle of multiple axes inside chart area.",
+					ChartType.OTHER));
+			list.add(new ChartSampleDescription("Good/Bad Chart",
+					"Example of filled area line chart with custom labels", ChartType.OTHER));
+			list.add(new ChartSampleDescription("ViewPager with Charts",
+					"Interactive charts within ViewPager. Each chart can be zoom/scroll except pie chart.",
+					ChartType.OTHER));
 
 			return list;
 		}
@@ -182,6 +199,7 @@ public class MainActivity extends ActionBarActivity {
 				holder = new ViewHolder();
 				holder.text1 = (TextView) convertView.findViewById(R.id.text1);
 				holder.text2 = (TextView) convertView.findViewById(R.id.text2);
+				holder.chartLayout = (FrameLayout) convertView.findViewById(R.id.chart_layout);
 
 				convertView.setTag(holder);
 			} else {
@@ -189,6 +207,44 @@ public class MainActivity extends ActionBarActivity {
 			}
 
 			ChartSampleDescription item = getItem(position);
+
+			holder.chartLayout.setVisibility(View.VISIBLE);
+			holder.chartLayout.removeAllViews();
+			AbstractChartView chart;
+			switch (item.chartType) {
+			case LINE_CHART:
+				chart = new LineChartView(getContext());
+				holder.chartLayout.addView(chart);
+				break;
+			case COLUMN_CHART:
+				chart = new ColumnChartView(getContext());
+				holder.chartLayout.addView(chart);
+				break;
+			case PIE_CHART:
+				chart = new PieChartView(getContext());
+				holder.chartLayout.addView(chart);
+				break;
+			case BUBBLE_CHART:
+				chart = new BubbleChartView(getContext());
+				holder.chartLayout.addView(chart);
+				break;
+			case PREVIEW_LINE_CHART:
+				chart = new PreviewLineChartView(getContext());
+				holder.chartLayout.addView(chart);
+				break;
+			case PREVIEW_COLUMN_CHART:
+				chart = new PreviewColumnChartView(getContext());
+				holder.chartLayout.addView(chart);
+				break;
+			default:
+				chart = null;
+				holder.chartLayout.setVisibility(View.GONE);
+				break;
+			}
+
+			if (null != chart) {
+				chart.setInteractive(false);// Disable touch handling for chart on the ListView.
+			}
 			holder.text1.setText(item.text1);
 			holder.text2.setText(item.text2);
 
@@ -199,6 +255,7 @@ public class MainActivity extends ActionBarActivity {
 
 			TextView text1;
 			TextView text2;
+			FrameLayout chartLayout;
 		}
 
 	}
@@ -206,11 +263,17 @@ public class MainActivity extends ActionBarActivity {
 	public static class ChartSampleDescription {
 		String text1;
 		String text2;
+		ChartType chartType;
 
-		public ChartSampleDescription(String text1, String text2) {
+		public ChartSampleDescription(String text1, String text2, ChartType chartType) {
 			this.text1 = text1;
 			this.text2 = text2;
+			this.chartType = chartType;
 		}
+	}
+
+	public enum ChartType {
+		LINE_CHART, COLUMN_CHART, PIE_CHART, BUBBLE_CHART, PREVIEW_LINE_CHART, PREVIEW_COLUMN_CHART, OTHER
 	}
 
 }
