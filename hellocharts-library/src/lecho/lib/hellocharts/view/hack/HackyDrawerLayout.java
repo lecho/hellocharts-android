1a08dcf38b2efd0c21ae264a55e5d03c74467563
Lecho <leszekwach@gmail.com>
2014-10-24 19:07:42 +0200

    Added HackyViewPager and HackyDrawerLayout to prevent errors with touch interception by ViewPager and DrawerLayout from support library

---
+++ hellocharts-library/src/lecho/lib/hellocharts/view/hack/HackyDrawerLayout.java
@@ -0,0 +1,36 @@
+package lecho.lib.hellocharts.view.hack;
+
+import android.content.Context;
+import android.support.v4.widget.DrawerLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Hacky fix for issue with DrawerLayout https://github.com/chrisbanes/PhotoView/issues/72
+ * 
+ */
+public class HackyDrawerLayout extends DrawerLayout {
+
+	public HackyDrawerLayout(Context context) {
+		super(context);
+	}
+
+	public HackyDrawerLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public HackyDrawerLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		try {
+			return super.onInterceptTouchEvent(ev);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+}
---
+++ hellocharts-library/src/lecho/lib/hellocharts/view/hack/HackyViewPager.java
@@ -0,0 +1,39 @@
+package lecho.lib.hellocharts.view.hack;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Hacky fix for Issue #4 and http://code.google.com/p/android/issues/detail?id=18990
+ * 
+ * ScaleGestureDetector seems to mess up the touch events, which means that ViewGroups which make use of
+ * onInterceptTouchEvent throw a lot of IllegalArgumentException: pointerIndex out of range.
+ * 
+ * There's not much I can do in my code for now, but we can mask the result by just catching the problem and ignoring
+ * it.
+ * 
+ * @author Chris Banes
+ */
+public class HackyViewPager extends ViewPager {
+
+	public HackyViewPager(Context context) {
+		super(context);
+	}
+
+	public HackyViewPager(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		try {
+			return super.onInterceptTouchEvent(ev);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+}
