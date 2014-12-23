ce050246d6d046e92670d9350f7f7d558f5a49b4
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2014-12-23 16:22:15 -0200

    - Fixed #35 - Created useful constructors for formatters

--- hellocharts-library/src/lecho/lib/hellocharts/formatter/SimpleLineChartValueFormatter.java
+++ hellocharts-library/src/lecho/lib/hellocharts/formatter/SimpleLineChartValueFormatter.java
@@ -4,7 +4,13 @@ import lecho.lib.hellocharts.model.PointValue;
 
 public class SimpleLineChartValueFormatter extends AbstractValueFormatter implements LineChartValueFormatter {
 
-	@Override
+    public SimpleLineChartValueFormatter(){};
+
+    public SimpleLineChartValueFormatter(int decimalDigitsNumber) {
+        setDecimalDigitsNumber(decimalDigitsNumber);
+    }
+
+    @Override
 	public int formatChartValue(char[] formattedValue, PointValue value) {
 		return super.formatFloatValueWithPrependedAndAppendedText(formattedValue, value.getY(), value.getLabel());
 	}
