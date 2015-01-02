53b921bb9df88799ed3c6a9792e04d77186e9b5e
Andres Torti <torti.max@gmail.com>
2015-01-02 20:42:02 -0300

    Added hashCode() and equals() method to PointValue which closes #45 (https://github.com/lecho/hellocharts-android/issues/45)

--- hellocharts-library/src/lecho/lib/hellocharts/model/PointValue.java
+++ hellocharts-library/src/lecho/lib/hellocharts/model/PointValue.java
@@ -53,4 +53,24 @@ public class PointValue implements Point {
 	public float getY() {
 		return this.y;
 	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PointValue)) return false;
+
+        PointValue that = (PointValue) o;
+
+        if (Float.compare(that.x, x) != 0) return false;
+        if (Float.compare(that.y, y) != 0) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (x != +0.0f ? Float.floatToIntBits(x) : 0);
+        result = 31 * result + (y != +0.0f ? Float.floatToIntBits(y) : 0);
+        return result;
+    }
 }
