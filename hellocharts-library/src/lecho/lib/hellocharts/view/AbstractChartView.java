7b1465d90dcb0f4ad266c6e6ae4040cab34c7582
Doug Melton <doug@mobiata.com>
2015-01-04 20:49:49 -0800

    Added setChartRenderer for easier overriding

--- hellocharts-library/src/lecho/lib/hellocharts/view/AbstractChartView.java
+++ hellocharts-library/src/lecho/lib/hellocharts/view/AbstractChartView.java
@@ -181,6 +181,13 @@ public abstract class AbstractChartView extends View implements Chart {
 	}
 
 	@Override
+	public void setChartRenderer(ChartRenderer renderer) {
+		chartRenderer = renderer;
+		chartRenderer.initDataMeasurements();
+		ViewCompat.postInvalidateOnAnimation(this);
+	}
+
+	@Override
 	public AxesRenderer getAxesRenderer() {
 		return axesRenderer;
 	}
