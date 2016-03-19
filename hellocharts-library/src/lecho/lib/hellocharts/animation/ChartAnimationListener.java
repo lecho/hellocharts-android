b99b07c09a6be1ec82724b56785d247d4f0573b6
ofirgeller <ofirgeller22@gmail.com>
2016-03-19 21:04:51 +0200

    Needes for xamarin binding to work
    Xamarin has problems when events have the same names making the binding fail. 
    this fixes it.

--- hellocharts-library/src/lecho/lib/hellocharts/animation/ChartAnimationListener.java
+++ hellocharts-library/src/lecho/lib/hellocharts/animation/ChartAnimationListener.java
@@ -4,7 +4,7 @@ package lecho.lib.hellocharts.animation;
  * Listener used to listen for chart animation start and stop events. Implementations of this interface can be used for
  * all types of chart animations(data, viewport, PieChart rotation).
  */
-public interface ChartAnimationListener {
+public interface ChartAnimationListener extends EventListener {
 
     public void onAnimationStarted();
 
