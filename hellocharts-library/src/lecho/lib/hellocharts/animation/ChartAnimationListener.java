46d888819f9132a55d79e35adbaa8b8700998897
Leszek Wach <leszekwach@gmail.com>
2016-03-21 21:11:32 +0100

    Fixed missing EventListener import

--- hellocharts-library/src/lecho/lib/hellocharts/animation/ChartAnimationListener.java
+++ hellocharts-library/src/lecho/lib/hellocharts/animation/ChartAnimationListener.java
@@ -1,5 +1,7 @@
 package lecho.lib.hellocharts.animation;
 
+import java.util.EventListener;
+
 /**
  * Listener used to listen for chart animation start and stop events. Implementations of this interface can be used for
  * all types of chart animations(data, viewport, PieChart rotation).
