767f47e99b60f4389518a516ea1cd51850f39201
Moritz Both <mb@aldebaran.de>
2016-06-07 16:54:08 +0200

    allow invalid Y values in LineChart (Float.NaN) resulting in interrupted line.

--- hellocharts-library/src/lecho/lib/hellocharts/renderer/LineChartRenderer.java
+++ hellocharts-library/src/lecho/lib/hellocharts/renderer/LineChartRenderer.java
@@ -215,15 +215,21 @@ public class LineChartRenderer extends AbstractChartRenderer {
         prepareLinePaint(line);
 
         int valueIndex = 0;
+        boolean invalidYValue = false;
         for (PointValue pointValue : line.getValues()) {
 
             final float rawX = computator.computeRawX(pointValue.getX());
             final float rawY = computator.computeRawY(pointValue.getY());
 
-            if (valueIndex == 0) {
-                path.moveTo(rawX, rawY);
-            } else {
-                path.lineTo(rawX, rawY);
+            if (Float.isNaN(rawY))
+                invalidYValue = true;
+            else {
+                if (valueIndex == 0 || invalidYValue) {
+                    path.moveTo(rawX, rawY);
+                } else {
+                    path.lineTo(rawX, rawY);
+                }
+                invalidYValue = false;
             }
 
             ++valueIndex;
