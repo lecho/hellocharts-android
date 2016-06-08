0048aa39fac1b55ec9ca764f4447ee3d41228dfc
Moritz Both <mb@aldebaran.de>
2016-06-08 10:13:15 +0200

    allow line interruptions (Float.NaN) for square lines, too. Note: For cubic lines this does not yet work.

--- hellocharts-library/src/lecho/lib/hellocharts/renderer/LineChartRenderer.java
+++ hellocharts-library/src/lecho/lib/hellocharts/renderer/LineChartRenderer.java
@@ -249,17 +249,19 @@ public class LineChartRenderer extends AbstractChartRenderer {
         prepareLinePaint(line);
 
         int valueIndex = 0;
-        float previousRawY = 0;
+        float previousRawY = Float.NaN;
         for (PointValue pointValue : line.getValues()) {
 
             final float rawX = computator.computeRawX(pointValue.getX());
             final float rawY = computator.computeRawY(pointValue.getY());
 
-            if (valueIndex == 0) {
-                path.moveTo(rawX, rawY);
-            } else {
-                path.lineTo(rawX, previousRawY);
-                path.lineTo(rawX, rawY);
+            if (! Float.isNaN(rawY)) {
+                if (Float.isNaN(previousRawY)) {
+                    path.moveTo(rawX, rawY);
+                } else {
+                    path.lineTo(rawX, previousRawY);
+                    path.lineTo(rawX, rawY);
+                }
             }
 
             previousRawY = rawY;
